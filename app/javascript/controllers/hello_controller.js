77f0ed6a422f9af1791363be5cf7af365f0e23ed
Tadesse-Alemayehu <tadalem26@gmail.com>
2022-12-23 07:48:37 +0300

    Toggle: input field visiblity

--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -1,7 +1,13 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
+  static targets = ["input-field"];
   connect() {
-    this.element.textContent = "Hello World!"
+    // this.element.textContent = "Hello World!";
+  }
+
+  toggleInput() {
+    console.log(this["input-fieldTarget"]);
+    this["input-fieldTarget"].hidden = !this["input-fieldTarget"].hidden;
   }
 }
